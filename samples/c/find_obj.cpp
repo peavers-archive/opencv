55f6b94690fb06a10c8bf6063b109466a1854f41
Vadim Pisarevsky <no@email>
2011-05-21 18:32:34 +0000

    replaced #include <some_opencv_hdr> with #include "some_opencv_hdr" (ticket #719)

--- samples/c/find_obj.cpp
+++ samples/c/find_obj.cpp
@@ -4,11 +4,11 @@
  * Author: Liu Liu
  * ***REDACTED-EMAIL***
  */
-#include <opencv2/core/core.hpp>
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
 #include <opencv2/imgproc/imgproc_c.h>
 
 #include <iostream>
